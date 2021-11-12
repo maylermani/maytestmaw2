9f444d3b0999e5c8cd6f17a25f949b1118eab4c3
Май Лермани <94169965+maylermani@users.noreply.github.com>
2021-11-12 17:16:18 +0000

    больше картинок мяо

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ img1 = 'https://static.wikia.nocookie.net/leagueoflegends/images/3/35/Kog%27Maw_
 img2 = 'https://static.wikia.nocookie.net/leagueoflegends/images/b/b2/Kog%27Maw_MonarchSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20181021120652'
 img3 = 'https://static.wikia.nocookie.net/leagueoflegends/images/0/01/Kog%27Maw_LionDanceSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20181021042930'
 img4 = 'https://static.wikia.nocookie.net/leagueoflegends/images/3/30/Kog%27Maw_JurassicSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20181021104244'
+img5 = 'https://static.wikia.nocookie.net/leagueoflegends/images/4/4e/Kog%27Maw_ArcanistSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20210922235242'
+img6 = 'https://static.wikia.nocookie.net/leagueoflegends/images/b/b1/Kog%27Maw_BattlecastSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20181021120624'
+img7 = 'https://static.wikia.nocookie.net/leagueoflegends/images/1/19/Kog%27Maw_OriginalSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20181021103708'
 
 @client.event
 async def on_ready():
@@ -19,6 +22,6 @@ async def on_message(message):
     return
 
   if message.content.startswith('!maw'):
-    await message.channel.send(random.choice([img2, img1, img3, img4]))
+    await message.channel.send(random.choice([img2, img1, img3, img4, img5, img6, img7]))
 
 client.run(os.getenv('TOKEN'))
