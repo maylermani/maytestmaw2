a5f45874b618d5cc6abec7c0ecb51be0f002960c
Май Лермани <94169965+maylermani@users.noreply.github.com>
2021-11-17 11:32:57 +0000

    другая картинка

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ img7 = 'https://static.wikia.nocookie.net/leagueoflegends/images/1/19/Kog%27Maw_
 baklazhan = "https://808.media/wp-content/uploads/2021/11/baklazhan-1.gif"
 
 ragnaros = "https://thumbs.gfycat.com/FlatElegantArmedcrab-size_restricted.gif"
-zluzyaCot = "https://www.meme-arsenal.com/memes/8d606add7d814760ede41413c027e5f0.jpg"
+zluzyaCot = "http://img2.joyreactor.cc/pics/comment/%D0%B3%D0%B8%D1%84%D0%BA%D0%B0-%D0%BA%D0%BE%D1%82%D1%8D-%D1%80%D1%83%D0%BA%D0%B0-%D0%B7%D0%B0%D0%B4%D1%83%D0%BC%D0%B0%D0%BB%D1%81%D1%8F-2422589.gif"
 
 @client.event
 async def on_ready():
