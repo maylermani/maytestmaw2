2d8aae66321aa5a2c8eeb7be5454ad17812ad7b1
Май Лермани <94169965+maylermani@users.noreply.github.com>
2021-11-17 10:45:20 +0000

    baklazhan

--- bot.py
+++ bot.py
@@ -12,6 +12,8 @@ img5 = 'https://static.wikia.nocookie.net/leagueoflegends/images/4/4e/Kog%27Maw_
 img6 = 'https://static.wikia.nocookie.net/leagueoflegends/images/b/b1/Kog%27Maw_BattlecastSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20181021120624'
 img7 = 'https://static.wikia.nocookie.net/leagueoflegends/images/1/19/Kog%27Maw_OriginalSkin.jpg/revision/latest/scale-to-width-down/1000?cb=20181021103708'
 
+baklazhan = "https://808.media/wp-content/uploads/2021/11/baklazhan-1.gif"
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -23,5 +25,8 @@ async def on_message(message):
 
   if message.content.startswith('!maw'):
     await message.channel.send(random.choice([img2, img1, img3, img4, img5, img6, img7]))
+
+  if message.content.startswith('!хуй'):
+    await message.channel.send(baklazhan)
 
 client.run(os.getenv('TOKEN'))
